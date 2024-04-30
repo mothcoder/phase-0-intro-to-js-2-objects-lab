190486c7b9f1adccb292e6634fa2e51ec481edc7
Athena Maynard <xfifix17@gmail.com>
2024-04-29 20:44:49 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Write your solution in this file!
+let employee = { 
+    name : "Sam", 
+    streetAddress: "11 Broadway, 2nd Floor, New York, NY 10004"
+};
+ 
+
+ 
+ 
+ function updateEmployeeWithKeyAndValue(employee, key, value) { 
+    
+    const newEmployee = {...employee }; 
+
+    newEmployee[key] = value; 
+
+    return newEmployee;
+
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+    
+    return employee;
+
+};
+
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const newEmployee = {...employee};
+
+    delete newEmployee[key];
+
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+    
+
+};
+
+
+    
+        
+            
+    
+    
+    
+ 
+
+      
